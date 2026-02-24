4d66a66101a0cc03ffbf643296825a8184f562be
Sulochana Acharya <acharyasulochana21@gmail.com>
2026-02-24 12:00:51 +0100

    heating-electricity design complete

--- electricity-frontend/src/app/pages/heating-electricity/heating-electricity.ts
+++ electricity-frontend/src/app/pages/heating-electricity/heating-electricity.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-heating-electricity',
-  imports: [],
+  imports: [CommonModule, MatIcon],
   templateUrl: './heating-electricity.html',
   styleUrl: './heating-electricity.css',
 })
 export class HeatingElectricity {
+  selectedOption: 'ja' | 'nein' = 'ja';
+  selectedTariff: 'single' | 'double' = 'single';
+  showNoBanner = false;
+
+  select(option: 'ja' | 'nein') {
+    this.selectedOption = option;
+
+  if (option === 'nein') {
+    this.selectedTariff = 'single';
+    this.showNoBanner = true;
+    } else {
+      this.showNoBanner = false;
+    }
+  }
+
+  closeNoBanner() {
+    this.showNoBanner = false;
+    this.selectedOption = 'ja'; // switches to JA view when X is clicked
+  }
+
+  tariff(type: 'single' | 'double') {
+    this.selectedTariff = type;
+  }
+
 
 }
